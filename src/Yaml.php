0ee5534d39f12cb52766ca712f14c09aedb8fce7
陈德华 <yunchat@yeah.net>
2018-07-06 20:09:51 +0800

    更新

---
+++ src/Yaml.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace OkamiChen\YamlConfig;
+
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml as Basic;
+
+/**
+ * Description of Yaml
+ *
+ * @author Administrator
+ */
+class Yaml {
+
+    /**
+     * 读取yaml配置
+     * @param string $file
+     * @param array $data
+     * @param string $env
+     */
+    static public function write($file, $data, $env = null) {
+
+        if (!$env) {
+            $env = env("APP_ENV");
+        }
+
+        $content = static::load($file, 'all');
+        $content[$env] = $data;
+        $yaml = Basic::dump($content, 4);
+        if (!is_writable($file)) {
+            $message = sprintf('File "%s" cannot be write.', $file);
+            throw new ParseException($message);
+        }
+
+        return file_put_contents($file, $yaml);
+    }
+    
+    /**
+     * 读取yaml配置
+     * @param string $file
+     * @param string $env
+     */
+    static public function load($file, $env=null){
+        if(!$env){
+            $env    = env("APP_ENV");
+        }
+        $yaml   = Basic::parseFile($file);
+        if($env == 'all'){
+            return $yaml;
+        }
+        return array_get($yaml, $env, []);
+    }
+
+}
